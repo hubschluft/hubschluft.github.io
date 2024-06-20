8dc778a9a555158a34172a8fcdd7876a4cc49f93
rendick <rendick123@proton.me>
2024-06-21 00:32:28 +0300

    New website

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+  
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+  
+  function count() {
+    var myMenu = document.getElementById("myMenu");
+    var listItems = myMenu.getElementsByTagName("li");
+    var countOutput = document.getElementById("countOutput");
+  
+    countOutput.textContent += listItems.length;
+  }
+  
+  count();
+  
+  function sort() {
+    var myMenu = document.getElementById("myMenu");
+    var listItems = myMenu.getElementsByTagName("li");
+    var itemsArray = Array.from(listItems);
+    itemsArray.sort(function (a, b) {
+      var textA = a.textContent || a.innerText;
+      var textB = b.textContent || b.innerText;
+      return textA.localeCompare(textB);
+    });
+  
+    myMenu.innerHTML = "";
+  
+    itemsArray.forEach(function (item) {
+      myMenu.appendChild(item);
+    });
+  }
+  
+  sort();
